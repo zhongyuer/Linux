1815850c684a61012961dbe831dd5d96186c8392
许立勣 <14340890+xu-liji1031@user.noreply.gitee.com>
2024-04-25 15:10:32 +0800

    update include

--- include/socket.h
+++ include/socket.h
@@ -14,10 +14,21 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 
-int socket_init(socket_t *sock, char *hostname, int port);
-int socket_connect(socket_t *sock);
-int socket_diag(socket_t *sock);
-int socket_write(socket_t *sock, char *data, int bytes);
-int socket_close(socket_t *sock);
+#define	HOSTNAME_LEN	64
+
+typedef struct socket_ctx_s
+{
+	char	host[HOSTNAME_LEN];
+	int		port;
+	int		fd;
+	int		connected;
+}socket_ctx_t;
+
+extern int socket_init(socket_ctx_t *sock, char *host, int port);
+extern int socket_connect(socket_ctx_t *sock);
+extern int socket_write(socket_ctx_t *sock, char *data);
+extern int socket_close(socket_ctx_t *sock);
+extern int socket_net_status(socket_ctx_t *sock);
+extern void set_socket_rlimit(void);
 
 #endif
