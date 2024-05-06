c37a51e5120601bc1b7a9a3892162887e05b2ad7
我的cpu爱发烫 <2589878706@qq.com>
2024-05-06 19:04:47 +0800

    update src_socket

--- project/src/socket.c
+++ project/src/socket.c
@@ -21,6 +21,7 @@
 #include <sys/resource.h>
 #include <netinet/tcp.h>
 #include <unistd.h>
+#include <netdb.h>
 #include "socket.h"
 
 
@@ -45,35 +46,75 @@ int socket_connect(socket_ctx_t *sock)
 	int					ret = 0;
 	int					sockfd = 0;
 	struct sockaddr_in	serv_addr;
+	struct addrinfo		*result;
+	struct addrinfo		*rp;
+	struct addrinfo		hinst;
+	struct in_addr		inaddr;		
+	int					ch = 0;;
+	char				port_str[32];
+	char				addr_str[64];
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
- 	if(sockfd<0)
-	{ 
-		printf("create socket failure: %s\n",strerror(errno));
-		return -1;
-	}	 
-	//printf("socket create fd[%d] seccessfully\n",sockfd);
-       
-	memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(sock->port);
-	inet_aton(sock->host, &serv_addr.sin_addr);
+	memset(&hinst, 0, sizeof(hinst));
+	hinst.ai_family = AF_INET;
+	hinst.ai_socktype = SOCK_STREAM;
+	//hinst.ai_flags = AI_CANONNAME;
+	hinst.ai_protocol = 0;
 
-	ret = connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-	if(ret <0 )
+	if(inet_aton(sock->host, &inaddr))
 	{
-		//printf("connect to server [%s:%d] failure: %s\n",sock->host, sock->port, strerror(errno));
-		close(sockfd);
+		hinst.ai_flags = AI_NUMERICHOST;
+		//printf("%s is a valid IP don't parser\n", sock->host);
+	}
+	
+	snprintf(port_str, sizeof(port_str), "%d", sock->port);
+	ch = getaddrinfo(sock->host, port_str, &hinst, &result);
+	if( ch != 0)
+	{
+		printf("analyze [%s,%s] failure: %s\n",sock->host, port_str, gai_strerror(errno));
 		return -1;
 	}
-	else
+	printf("analyze successfully\n");
+
+	for(rp = result; rp != NULL; rp=rp->ai_next)
 	{
-		sock->fd = sockfd;
-		printf("connect to server [%s:%d] on fd[%d] successfully\n",sock->host, sock->port, sockfd);
+		char				ipaddr[32];
+		struct sockaddr_in	*sp = (struct sockaddr_in *) rp->ai_addr;
+
+		memset(ipaddr,0, sizeof(ipaddr));
+		if( inet_ntop(AF_INET, &sp->sin_addr, ipaddr, sizeof(ipaddr)))
+		{
+			printf("domain name resolution [%s->%s]\n", sock->host, ipaddr);
+
+		}
+		
+		sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+		if(sockfd <0 )
+		{
+			printf("create socket failure: %s\n",gai_strerror(errno));
+			continue;
+		}
+	
+		ret = connect(sockfd, rp->ai_addr, rp->ai_addrlen);
+		if(ret <0 )
+		{
+			//printf("connect to server [%s:%d] failure: %s\n",sock->host, sock->port, strerror(errno));
+			close(sockfd);
+			continue;
+		}
+		else
+		{
+			sock->fd = sockfd;
+			printf("connect to server [%s:%d] on fd[%d] successfully\n", sock->host, sock->port, sockfd);
+			inet_ntop(rp->ai_family, rp->ai_addr, addr_str, sizeof(addr_str));
+			printf("IP address: %s\n", addr_str);
+			break;
+		}
 	}
 
+	freeaddrinfo(result);
 	return 0;
 }
+
 
 int socket_write(socket_ctx_t *sock, char *data)
 {
